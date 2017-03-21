e4a414a58e6d9a9229768d75212f8b33769885b1
adamh <slayer@plusnet.pl>
2017-03-21 20:38:57 +0100

    deleted images

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
 				files: [{
 					expand: true,
 					cwd: 'images/',
-					src: ['**/*.{png,jpg,gif}'],
+					src: ['*.{png,jpg,gif}'],
 					dest: 'images/build/'
 				}]
 			}
